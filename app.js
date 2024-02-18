c22d3ba106be8dac79ed92a95b1b1c033ef70e33
Jay Keer <jaykeer@Jays-MacBook-Air.local>
2024-02-18 20:03:59 +0530

    replaced ssh login with basic login

--- app.js
+++ app.js
@@ -39,42 +39,16 @@ io.on('connection', socket => {
 });
 
 
-//SSH LOGIC
+//Login Logic
 app.post('/', function (req, res) {
     const username = req.body.username;
     const password = req.body.pass;
-    const ip_address = req.body.ip_address;
 
-    // SSH Connection options
-    const sshConfig = {
-        host: ip_address,
-        port: 22,
-        username: username,
-        password: password
-    };
-
-    const conn = new Client();
-
-    // Connect to the SSH server
-    conn.on('ready', function () {
-        console.log('SSH connection established.');
-
-        // Once connected, you can execute commands
-        conn.exec('ls', function (err, stream) {
-            if (err) throw err;
-
-            stream.on('close', function (code, signal) {
-                console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-                conn.end(); // Close the connection once done
-            }).on('data', function (data) {
-                console.log('STDOUT: ' + data);
-                // Here you can send data back to the client if needed
-            }).stderr.on('data', function (data) {
-                console.log('STDERR: ' + data);
-                // Handle STDERR data
-            });
-        });
-    }).connect(sshConfig);
+    if(username=='admin' && password=='ET132'){
+        res.redirect('/cam-feed');
+    }else{
+        res.send("INCORRECT PASSWORD BOZO!");
+    }
 });
   
  
