e0b198bebc1ef1f401296d35b052ecfb6f764add
Jay Keer <jaykeer@Jays-MacBook-Air.local>
2024-03-02 17:15:59 +0530

    Radar graph update

--- app.js
+++ app.js
@@ -1,72 +1,78 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
-const path = require('path');
-const fs = require('fs');
-const io = require('socket.io')(http);
-
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const path = require("path");
+const fs = require("fs");
+const io = require("socket.io")(http);
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const socketio = require('socket.io')(server);
-const server_ip = '192.168.238.205';
+const socketio = require("socket.io")(server);
+const server_ip = "192.168.1.101";
 let receivedFrameData = null;
-
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/cam-feed', function(req, res) {
-    res.render('home');
+app.get("/cam-feed", function (req, res) {
+  res.render("home");
 });
 
-app.get('/', function(req, res) {
-    res.render('login');
+app.get("/", function (req, res) {
+  res.render("login");
 });
 
-app.get('/receiver', function(req, res) {
-    res.render('receiver', { imageData: receivedFrameData });
+app.get("/receiver", function (req, res) {
+  var numbersData = generateNumbersData();
+  res.render("receiver", { imageData: receivedFrameData },)
 });
 
-app.post('/', function (req, res) {
-    const username = req.body.username;
-    const password = req.body.pass;
-    const clientIP = req.ip;
+function generateNumbersData() {
+  const numbersData = [];
+  for (let i = 0; i <= 360; i++) {
+      const randomNumber = Math.floor(Math.random() * 10) + 1;
+      numbersData.push({ number: i, randomNumber: randomNumber });
+  }
+  return numbersData;
+}
 
-    if(username === 'admin' && password === 'ET132'){
-        if(clientIP === server_ip){
-            res.redirect('/cam-feed');
-        }else{
-            res.redirect("/receiver");
-        }
+app.post("/", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.pass;
+  const clientIP = req.ip;
+
+  if (username === "admin" && password === "ET132") {
+    if (clientIP === server_ip) {
+      res.redirect("/cam-feed");
     } else {
-        res.send("INCORRECT PASSWORD BOZO!");
+      res.redirect("/receiver");
     }
+  } else {
+    res.send("INCORRECT PASSWORD BOZO!");
+  }
 });
 
-wss.on('connection', function connection(ws) {
-    console.log('Client connected.');
+wss.on("connection", function connection(ws) {
+  console.log("Client connected.");
 
-    ws.on('message', function incoming(imageData) {
-        //console.log('Received frame from client.');
-        //console.log(imageData);
-        receivedFrameData = imageData;
+  ws.on("message", function incoming(imageData) {
+    //console.log('Received frame from client.');
+    //console.log(imageData);
+    receivedFrameData = imageData;
 
-        
-        
-        socketio.emit('frame', imageData);
-    });
+    socketio.emit("frame", imageData);
+  });
 
-    ws.on('close', function close() {
-        console.log('Client disconnected.');
-    });
+  ws.on("close", function close() {
+    console.log("Client disconnected.");
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
