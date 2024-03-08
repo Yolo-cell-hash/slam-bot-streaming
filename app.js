6be29783db45d3f80fdfd731df90709c4278caa6
Jay Keer <jaykeer@Jays-MacBook-Air.local>
2024-03-08 20:24:20 +0530

    Stable Version

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const socketio = require("socket.io")(server);
-const server_ip = "192.168.1.100";
+const server_ip = "192.168.0.109";
 let receivedFrameData = null;
 let numbersData=[];
 
